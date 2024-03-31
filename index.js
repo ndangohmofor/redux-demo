93a88b05ee6d17f717aa4e54e881e60d1e9cf87d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-03-31 15:38:06 -0400

    accounting for other properties in the state

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
+        ...state,
         numOfCakes: state.numOfCakes - 1,
       };
     default:
