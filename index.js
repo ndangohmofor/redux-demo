f8adcaff71a3be1307af450ca07e1afdac3411f2
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-01 06:04:50 -0400

    using one reducer to manage different states

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const redux = require("redux");
 const createStore = redux.createStore;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -10,10 +11,17 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
 // (previousState, action) => newState
 
 const initialState = {
   numOfCakes: 10,
+  numOfIceCreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -22,6 +30,11 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
+      };
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
       };
     default:
       return state;
@@ -36,4 +49,6 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 unsubscribe();
