e880b286906ca1e7cfd234cca7c5725591879381
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-01 06:33:44 -0400

    create setup for new state and actions for fetching users from an api

---
+++ asyncActions.js
@@ -0,0 +1,29 @@
+const FETCH_USERS_REQUEST = "FETCH_USERS_REQUEST";
+const FETCH_USERS_SUCCESS = "FETCH_USERS_SUCCESS";
+const FETCH_USERS_FAILURE = "FETCH_USERS_FAILURE";
+
+const initialState = {
+  loading: false,
+  users: [],
+  errors: "",
+};
+
+const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST,
+  };
+};
+
+const fetchUsersSuccess = (users) => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users,
+  };
+};
+
+const fetchUsersFailure = (error) => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error,
+  };
+};
