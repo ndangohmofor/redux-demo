54839ba0bd38dbad6bdcbf3e820e7183d2767e69
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-04-01 06:13:35 -0400

    combining two reducers to form a combined reducer called rootReducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
 const BUY_ICECREAM = "BUY_ICECREAM";
@@ -61,7 +62,7 @@ const cakeReducer = (state = initialCakeState, action) => {
   }
 };
 
-const buyIceCreamReducer = (state = initialIceCreamState, action) => {
+const iceCreamReducer = (state = initialIceCreamState, action) => {
   switch (action.type) {
     case BUY_ICECREAM:
       return {
@@ -73,7 +74,11 @@ const buyIceCreamReducer = (state = initialIceCreamState, action) => {
   }
 };
 
-const store = createStore(reducer);
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
 console.log("Initial state", store.getState());
 const unsubscribe = store.subscribe(() =>
   console.log("Updated state", store.getState())
